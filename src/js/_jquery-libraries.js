cb6f5da95930ba051bf43dfa9b10e06fa21a80ca
Mary Zin <zin.marie.v@gmail.com>
2024-09-18 13:08:13 +0300

    feat: remove ui-juery-slider

--- src/js/_jquery-libraries.js
+++ src/js/_jquery-libraries.js
@@ -1,5 +1,4 @@
 import validate from 'jquery-validation';
 import niceNumber from '../../node_modules/jquery.nice-number/dist/jquery.nice-number.js';
-import jqueryUiSlider from '../../node_modules/jquery-ui-slider/jquery-ui.js';
 
 
--- src/partials/components/range-slider/range-slider.js
+++ src/partials/components/range-slider/range-slider.js
@@ -1,11 +1,1 @@
-$( '.range-slider__slider' ).slider({
-  animate: true,
-  range: true,
-  min: 0,
-  max: 15000,
-  values: ['5000', '10000'],
-  slide: function(event, ui) {
-    $('#min').val(ui.values[0]);
-    $('#max').val(ui.values[1]);
-  }
-});
+
