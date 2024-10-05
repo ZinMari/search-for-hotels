0f1c08a7a70d3be1bcd03dac7afe0da7822cebcc
Mary Zin <zin.marie.v@gmail.com>
2024-10-05 20:04:46 +0300

    development of logic for displaying the number of selected dropdown items

--- src/partials/components/dropdown/dropdown.js
+++ src/partials/components/dropdown/dropdown.js
@@ -1,6 +1,15 @@
 const dropBtns = document.querySelectorAll('.dropdown__btn');
 const dropInputs = document.querySelectorAll('.dropdown__input');
 
+const dictionary = [
+  ['спальня', 'спальни', 'спален'],
+  ['кровать', 'кровати', 'кроватей'],
+  ['ванная комната', 'ванные комнаты', 'ванных комнат'],
+  ['гость', 'гостя', 'гостей'],
+  ['младенец', 'младенца', 'младенцев'],
+]
+
+
 
 dropBtns.forEach(dropBtn => {
   dropBtn.addEventListener('click', function(){
@@ -9,19 +18,31 @@ dropBtns.forEach(dropBtn => {
   })
 })
 
+function setTitle(currentInput){
+  const parent = currentInput.closest('.dropdown');
+  const title = $('.dropdown__btn span', parent).text();
+  const labels = parent.find('.dropdown__label');
+
+  console.log(currentInput.closest('.dropdown__label').find('span').text());
+}
+
 $('.dropdown__input').niceNumber({
   autoSize: false,
   onIncrement: function (currentInput, amount) {
     currentInput.prev().removeClass('nice-number__button--disabed')
+
     if (amount == currentInput.attr('max')) {
       currentInput.next().addClass('nice-number__button--disabed')
     }
+
+    setTitle(currentInput)
   },
   onDecrement: function (currentInput, amount) {
     currentInput.next().removeClass('nice-number__button--disabed')
     if (amount == currentInput.attr('min')) {
       currentInput.prev().addClass('nice-number__button--disabed')
     }
+    setTitle(currentInput)
   },
 });
 
