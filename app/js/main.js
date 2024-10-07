ea73d0d0658af864ac6a19ed200139d5249aca43
Mari Zin <zin.marie.v@gmail.com>
2024-10-07 07:54:15 +0300

    feat: update project

--- app/js/main.js
+++ app/js/main.js
@@ -22998,12 +22998,52 @@ __webpack_require__.r(__webpack_exports__);
 /* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 const dropBtns = document.querySelectorAll('.dropdown__btn');
 const dropInputs = document.querySelectorAll('.dropdown__input');
+const dictionary = {
+  'спальни': ['спальня', 'спальни', 'спален'],
+  'кровати': ['кровать', 'кровати', 'кроватей'],
+  'ванные комнаты': ['ванная комната', 'ванные комнаты', 'ванных комнат'],
+  'взрослые': ['гость', 'гостя', 'гостей'],
+  'младенцы': ['младенец', 'младенца', 'младенцев']
+};
+function definitionOfDeclension(count) {
+  let declension;
+  if (count == 1) {
+    declension = 0;
+  } else if (count >= 2 && count <= 4) {
+    declension = 1;
+  } else if (count == 0 || count >= 4) {
+    declension = 2;
+  }
+  return declension;
+}
 dropBtns.forEach(dropBtn => {
   dropBtn.addEventListener('click', function () {
     this.ariaExpanded = this.ariaExpanded === 'false' ? true : false;
     this.closest('.dropdown').classList.toggle('dropdown--open');
   });
 });
+function setTitle(currentInput, amount) {
+  const parent = currentInput.closest('.dropdown');
+  const title = $('.dropdown__btn span', parent);
+  const labels = parent.find('.dropdown__label');
+  const values = {};
+  labels.each(function (index, elem) {
+    const elemText = $('span', elem).text();
+    if (elemText === 'дети' || elemText === 'взрослые') {
+      values['взрослые'] = 'взрослые' in values ? Number(values['взрослые']) + Number($('.dropdown__input', elem).val()) : Number($('.dropdown__input', elem).val());
+    } else {
+      values[elemText] = $('.dropdown__input', elem).val();
+    }
+  });
+  let text = [];
+  $.each(values, function (key, value) {
+    if (+value !== 0) {
+      const declension = definitionOfDeclension(value);
+      text.push(`${value} ${dictionary[key][declension]}`);
+    }
+  });
+  title.text(text.length === 0 ? 'Выберете количество' : text.join(', '));
+}
 $('.dropdown__input').niceNumber({
   autoSize: false,
   onIncrement: function (currentInput, amount) {
@@ -23011,12 +23051,14 @@ $('.dropdown__input').niceNumber({
     if (amount == currentInput.attr('max')) {
       currentInput.next().addClass('nice-number__button--disabed');
     }
+    setTitle(currentInput);
   },
   onDecrement: function (currentInput, amount) {
     currentInput.next().removeClass('nice-number__button--disabed');
     if (amount == currentInput.attr('min')) {
       currentInput.prev().addClass('nice-number__button--disabed');
     }
+    setTitle(currentInput);
   }
 });
 dropInputs.forEach(dropInput => {
@@ -23115,33 +23157,7 @@ glides.forEach(elem => {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
-$("#subscr-form").validate({
-  errorPlacement: function (error) {
-    error.insertAfter($('.subscr-form__wrapp'));
-  },
-  errorClass: 'subscr-form__error',
-  // highlight: function(element, validClass) {
-  //   $(element).addClass('sdfsdfsdfsdf').removeClass(validClass);
-  // },
 
-  // unhighlight: function(element, errorClass, validClass) {
-  //   $(element).removeClass(errorClass).addClass(validClass);
-  // },
-  debug: true,
-  rules: {
-    email_subscr: {
-      required: true,
-      email: true
-    }
-  },
-  messages: {
-    email_subscr: {
-      required: "Введите e-mail!",
-      email: "Адрес должен быть вида name@domain.com"
-    }
-  }
-});
 
 /***/ }),
 
