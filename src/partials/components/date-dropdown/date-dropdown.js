da018cbc51254898fc11952f1143a862a40d6d23
Mary Zin <zin.marie.v@gmail.com>
2024-10-09 08:38:13 +0300

    fix: synchronous operation of all calendars

--- src/partials/components/date-dropdown/date-dropdown.js
+++ src/partials/components/date-dropdown/date-dropdown.js
@@ -1,19 +1,21 @@
 import AirDatepicker from 'air-datepicker';
 
 const dateBtns = document.querySelectorAll('.date-dropdown__btn');
-const calendarInput = document.querySelector('.date-dropdown__calendar');
+const calendarInputs = document.querySelectorAll('.date-dropdown__calendar');
 
 const applyBtn = {
   content: 'Применить',
   className: 'applyBtn .date-dropdown__action-btn',
   onClick: (dp) => {
     if(dp.selectedDates.length) {
-      dateBtns.forEach(btn => {
-        const templateDate = {day: '2-digit', month: '2-digit', year: 'numeric'};
 
-        const startDate = dp.selectedDates[0].toLocaleString('ru', templateDate);
-        const endDate = dp.selectedDates[1].toLocaleString('ru', templateDate);
+      const identifierCalendar = dp.$el.dataset.identifier;
+      const templateDate = {day: '2-digit', month: '2-digit', year: 'numeric'};
+      const startDate = dp.selectedDates[0].toLocaleString('ru', templateDate);
+      const endDate = dp.selectedDates[1].toLocaleString('ru', templateDate);
 
+      const showDateBtns = document.querySelectorAll(`button[data-identifier=${identifierCalendar}]`)
+      showDateBtns.forEach(btn=>{
         if(btn.dataset.value === "start") {
           btn.firstElementChild.textContent = startDate;
         } else {
@@ -30,14 +32,17 @@ const clearBtn = {
   content: 'Очистить',
   className: 'clearBtn',
   onClick: (dp) => {
+    const identifierCalendar = dp.$el.dataset.identifier;
+    const showDateBtns = document.querySelectorAll(`button[data-identifier=${identifierCalendar}]`)
     dp.clear();
-    dateBtns.forEach(btn => {
+
+    showDateBtns.forEach(btn => {
       btn.firstElementChild.textContent = 'ДД.ММ.ГГГГ'
     })
   }
 }
 
-const calendarOptions = {
+const commonCalendarOptions = {
   range: true,
   buttons: [clearBtn, applyBtn],
   nextHtml: '',
@@ -53,17 +58,32 @@ const calendarOptions = {
       }
     }
   },
-  // minDate: new Date(),
   classes: 'date-dropdown__datepicker',
 };
 
-const calendarElement = new AirDatepicker(calendarInput, calendarOptions);
+const filterCalendarOptions = {
+  multipleDatesSeparator: ' - ',
+  dateFormat: 'dd MMM',
+}
+
+const calendars = [];
+
+calendarInputs.forEach(calendar => {
+  calendar.classList.add(calendar.dataset.identifier)
+  const calendarElement = new AirDatepicker(calendar, commonCalendarOptions);
+  calendars.push(calendarElement)
+})
+
 
 function showCalendar() {
-  if(calendarElement.visible){
-    calendarElement.hide();
-  } else {
-    calendarElement.show();
+  for(let calendar of calendars) {
+    if(calendar.$el.classList.contains(this.dataset.identifier)){
+      if(calendar.visible){
+        calendar.hide();
+      } else {
+        calendar.show();
+      }
+    }
   }
 }
 
