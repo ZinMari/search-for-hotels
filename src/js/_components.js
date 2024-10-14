65099b566f76b44dad74d746a763cdf70ff64fa4
Mari Zin <zin.marie.v@gmail.com>
2024-10-14 13:48:12 +0300

    refactor: remove date-dropdown.js file

--- src/js/_components.js
+++ src/js/_components.js
@@ -5,6 +5,5 @@ import '../partials/components/range-slider/range-slider.js';
 import '../partials/components/expandable-checkbox-list/expandable-checkbox-list.js';
 import '../partials/components/datepicker/datepicker.js';
 import '../partials/components/text-field/text-field.js';
-import '../partials/components/date-dropdown/date-dropdown.js';
 import '../partials/components/room-card/room-card.js';
 import '../partials/components/header/header.js';
