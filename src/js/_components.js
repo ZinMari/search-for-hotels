79694a3e8b398266f10a923c6817f06c51d4d6f3
Mari Zin <zin.marie.v@gmail.com>
2024-10-14 12:58:42 +0300

    feat: remove background-slider component and all of its uses

--- src/js/_components.js
+++ src/js/_components.js
@@ -7,5 +7,4 @@ import '../partials/components/datepicker/datepicker.js';
 import '../partials/components/text-field/text-field.js';
 import '../partials/components/date-dropdown/date-dropdown.js';
 import '../partials/components/room-card/room-card.js';
-import '../partials/components/background-slider/background-slider.js';
 import '../partials/components/header/header.js';
