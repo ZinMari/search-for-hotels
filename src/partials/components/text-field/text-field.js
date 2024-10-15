3c6214b29fbbbe877135ceecd5c54f6f66ba450a
Mari Zin <zin.marie.v@gmail.com>
2024-10-15 11:45:07 +0300

    refactor: delete text-field.js

--- src/partials/components/text-field/text-field.js
+++
@@ -1,9 +0,0 @@
-// const input = document.querySelector('.text-field__input');
-
-// Inputmask({
-//   // "mask": "ДД.ММ.ГГГГ",
-//   // onincomplete: function() {
-//   //   $(this).val('');
-//   // },
-// })
-//   .mask(input);
