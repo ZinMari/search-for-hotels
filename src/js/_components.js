907bb818057a67fcf220db4134cf2ec553a4df23
Mary Zin <zin.marie.v@gmail.com>
2024-10-17 05:49:12 +0300

    fix: remove links to deleted components in _components.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,9 +1,8 @@
-import '../partials/components/subscription-text-field/subscription-text-field.js';
 import '../partials/components/dropdown/dropdown.js';
 import '../partials/components/like-button/like-button.js';
 import '../partials/components/range-slider/range-slider.js';
 import '../partials/components/expandable-checkbox-list/expandable-checkbox-list.js';
 import '../partials/components/datepicker/datepicker.js';
-import '../partials/components/text-field/text-field.js';
 import '../partials/components/room-card/room-card.js';
 import '../partials/components/header/header.js';
+
