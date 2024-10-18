da2144d4cb75991bacd8809cbb73a57d9355e02c
Mari Zin <zin.marie.v@gmail.com>
2024-10-18 12:12:37 +0300

    feat: add function for background slider

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,27 @@ import './_components.js';
 
 
 
+(function backgroundSlider(){
+  const indexPageSection = document.querySelector('.start');
+  // const registrationPageSection = document.querySelector('.start');
+  // const signInPageSection = document.querySelector('.start');
+
+  const sliderIndex = ['../img/index-background/main-slider-1.jpg', '../img/index-background/main-slider-2.jpg', '../img/index-background/main-slider-3.jpg'];
+  const sliderRegistrationSignIn = ['../img/registration-sign-in-background/slide-1.jpg', '../img/registration-sign-in-background/slide-2.jpg'];
+
+
+
+  function slider(elem, imagesArr) {
+    let index = 0;
+
+    // setInterval(()=>{
+    //   elem.style.backgroundImage = `url('${imagesArr[index]}')`;
+    //   index++;
+    //   if(index === imagesArr.length) index = 0;
+    // }, 5000)
+  }
+
+  if(indexPageSection)slider(indexPageSection, sliderIndex)
+  // if(registrationPageSection)slider(registrationPageSection, sliderRegistrationSignIn)
+  // if(signInPageSection)slider(signInPageSection, sliderIndex)
+})()
