266289330077b66f9ca410dedbcc3b5fc96987ca
Mari Zin <zin.marie.v@gmail.com>
2025-03-24 12:28:03 +0300

    refactor: imports place

--- src/js/_jquery-libraries.js
+++ src/js/_jquery-libraries.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import validate from 'jquery-validation';
 import rangeSlider from 'ion-rangeslider'
-import niceNumber from '../../node_modules/jquery.nice-number/dist/jquery.nice-number.js';
-
 import inputmask from "inputmask";
+import niceNumber from '../../node_modules/jquery.nice-number/dist/jquery.nice-number.js';
