665f862fa7d855512cccb5fc32c45c8e1be0d656
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-10 11:17:34 +0300

    fix: #1 bem naming in template-main

--- src/partials/components/header/header.js
+++ src/partials/components/header/header.js
@@ -1,11 +1,11 @@
-const $burgerBtn = $('.js-header__burger');
-const $headerMenu = $('.js-header__menu');
+const $burgerBtn = $(".js-header__burger");
+const $headerMenu = $(".js-header__menu");
 
-function handleBurgerClick(){
-  $burgerBtn.toggleClass('header__burger--open');
-  $burgerBtn.children().toggleClass('header__burger-line--open');
-  $headerMenu.toggleClass('header__menu--open');
-  $('html').toggleClass('page--menu-open');
+function handleBurgerClick() {
+  $burgerBtn.toggleClass("header__burger--open");
+  $burgerBtn.children().toggleClass("header__burger-line--open");
+  $headerMenu.toggleClass("header__menu--open");
+  $("html").toggleClass("page_menu_open");
 }
 
-$burgerBtn.on('click.header', handleBurgerClick)
+$burgerBtn.on("click.header", handleBurgerClick);
