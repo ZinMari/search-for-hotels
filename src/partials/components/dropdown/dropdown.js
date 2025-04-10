c094acebc4c4086037727a11ea08ad3068d26b63
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-10 11:37:40 +0300

    fix: #1 bem naming in the dropdown component

--- src/partials/components/dropdown/dropdown.js
+++ src/partials/components/dropdown/dropdown.js
@@ -1,143 +1,144 @@
-const $dropBtns = $('.js-dropdown__btn');
-const $dropInputs = $('js-dropdown__input');
-const $dropdowns = $('.js-dropdown');
-const $dropClearBtns = $('.dropdown__clear button');
+const $dropBtns = $(".js-dropdown__btn");
+const $dropInputs = $("js-dropdown__input");
+const $dropdowns = $(".js-dropdown");
+const $dropClearBtns = $(".dropdown__clear button");
 
+$dropClearBtns.on("click.dropdown", resetValues);
 
-$dropClearBtns.on('click.dropdown', resetValues);
+function resetValues() {
+  const parent = this.closest(".js-dropdown");
+  const $inputs = $(".js-dropdown__input", parent);
+  const $title = $(".js-dropdown__btn span", parent);
 
+  $inputs.each((index, input) => {
+    $(input).val("0");
+    $(input).prev().addClass("nice-number__button_disabed");
+  });
 
-function resetValues(){
-  const parent = this.closest('.js-dropdown');
-  const $inputs = $('.js-dropdown__input', parent);
-  const $title = $('.js-dropdown__btn span', parent);
-
-  $inputs.each((index, input)=>{
-    $(input).val('0');
-    $(input).prev().addClass('nice-number__button--disabed');
-  })
-
-  $title.text('Выберете количество');
-  $('span', this).attr('hidden', true);
+  $title.text("Выберете количество");
+  $("span", this).attr("hidden", true);
 }
 
-
-$dropdowns.each(function(){
-  setButtonClear(this)
-})
+$dropdowns.each(function () {
+  setButtonClear(this);
+});
 
 function isEmptyValues(dropdown) {
-  const sum = Object.values(getData(dropdown)).reduce((a,b)=> Number(a) +  Number(b), 0);
+  const sum = Object.values(getData(dropdown)).reduce(
+    (a, b) => Number(a) + Number(b),
+    0,
+  );
   return sum === 0;
 }
 
 const dictionary = {
-  'спальни': ['спальня', 'спальни', 'спален'],
-  'кровати': ['кровать', 'кровати', 'кроватей'],
-  'ванные комнаты': ['ванная комната', 'ванные комнаты', 'ванных комнат'],
-  'взрослые': ['гость', 'гостя', 'гостей'],
-  'младенцы': ['младенец', 'младенца', 'младенцев'],
-}
+  спальни: ["спальня", "спальни", "спален"],
+  кровати: ["кровать", "кровати", "кроватей"],
+  "ванные комнаты": ["ванная комната", "ванные комнаты", "ванных комнат"],
+  взрослые: ["гость", "гостя", "гостей"],
+  младенцы: ["младенец", "младенца", "младенцев"],
+};
 
-function definitionOfDeclension(count){
+function definitionOfDeclension(count) {
   let declension;
 
-  if(count == 1) {
+  if (count == 1) {
     declension = 0;
-  } else if(count >= 2 && count <= 4) {
+  } else if (count >= 2 && count <= 4) {
     declension = 1;
-  } else if(count == 0 || count >= 4) {
+  } else if (count == 0 || count >= 4) {
     declension = 2;
   }
 
   return declension;
 }
 
-function handleDropBtnsClick (e){
+function handleDropBtnsClick(e) {
   e.preventDefault();
-  e.currentTarget.closest('.js-dropdown').classList.toggle('dropdown_open');
+  e.currentTarget.closest(".js-dropdown").classList.toggle("dropdown_open");
 }
 
-$dropBtns.on('click.dropdown', handleDropBtnsClick)
+$dropBtns.on("click.dropdown", handleDropBtnsClick);
 
-function setButtonClear(currentInput){
-  const parent = currentInput.closest('.js-dropdown');
-  const $buttonClear = $('.dropdown__clear span', parent);
+function setButtonClear(currentInput) {
+  const parent = currentInput.closest(".js-dropdown");
+  const $buttonClear = $(".dropdown__clear span", parent);
 
-  if(isEmptyValues(currentInput)) {
-    $buttonClear.attr('hidden', true)
+  if (isEmptyValues(currentInput)) {
+    $buttonClear.attr("hidden", true);
   } else {
-    $buttonClear.attr('hidden', false)
+    $buttonClear.attr("hidden", false);
   }
 }
 
-function getData(currentInput){
-  const parent = currentInput.closest('.js-dropdown');
-  const $labels = $('.dropdown__label', parent);
+function getData(currentInput) {
+  const parent = currentInput.closest(".js-dropdown");
+  const $labels = $(".dropdown__label", parent);
   const values = {};
 
-  $labels.each(function(index, elem){
-    const elemText = $('span', elem).text();
+  $labels.each(function (index, elem) {
+    const elemText = $("span", elem).text();
 
-    if(elemText === 'дети' || elemText === 'взрослые'){
-      values['взрослые'] =
-      'взрослые' in values
-      ? Number(values['взрослые']) + Number($('.js-dropdown__input', elem).val())
-      : Number($('.js-dropdown__input', elem).val());
+    if (elemText === "дети" || elemText === "взрослые") {
+      values["взрослые"] =
+        "взрослые" in values
+          ? Number(values["взрослые"]) +
+            Number($(".js-dropdown__input", elem).val())
+          : Number($(".js-dropdown__input", elem).val());
     } else {
-      values[elemText] = $('.js-dropdown__input', elem).val();
+      values[elemText] = $(".js-dropdown__input", elem).val();
     }
   });
 
   return values;
 }
 
-function setTitle(elem, values){
-  const parent = elem.closest('.js-dropdown');
-  const $title = $('.js-dropdown__btn span', parent);
+function setTitle(elem, values) {
+  const parent = elem.closest(".js-dropdown");
+  const $title = $(".js-dropdown__btn span", parent);
   let text = [];
 
-  $.each(values, function(key, value){
-      if(+value !== 0) {
-        const declension = definitionOfDeclension(value);
-        text.push(`${value} ${dictionary[key][declension]}`)
-      }
-  })
+  $.each(values, function (key, value) {
+    if (+value !== 0) {
+      const declension = definitionOfDeclension(value);
+      text.push(`${value} ${dictionary[key][declension]}`);
+    }
+  });
 
-  $title.text(text.length === 0 ? 'Выберете количество' : text.join(', '))
+  $title.text(text.length === 0 ? "Выберете количество" : text.join(", "));
 }
 
-$('.js-dropdown__input').niceNumber({
+$(".js-dropdown__input").niceNumber({
   autoSize: false,
   onIncrement: function (currentInput, amount) {
-    currentInput.prev().removeClass('nice-number__button--disabed')
+    currentInput.prev().removeClass("nice-number__button_disabed");
 
-    if (amount == currentInput.attr('max')) {
-      currentInput.next().addClass('nice-number__button--disabed')
+    if (amount == currentInput.attr("max")) {
+      currentInput.next().addClass("nice-number__button_disabed");
     }
 
     setTitle(currentInput, getData(currentInput));
-    setButtonClear(currentInput)
+    setButtonClear(currentInput);
   },
 
   onDecrement: function (currentInput, amount) {
-    currentInput.next().removeClass('nice-number__button--disabed')
-    if (amount == currentInput.attr('min')) {
-      currentInput.prev().addClass('nice-number__button--disabed');
+    currentInput.next().removeClass("nice-number__button_disabed");
+    if (amount == currentInput.attr("min")) {
+      currentInput.prev().addClass("nice-number__button_disabed");
     }
 
     setTitle(currentInput, getData(currentInput));
-    setButtonClear(currentInput)
+    setButtonClear(currentInput);
   },
 });
 
-$dropInputs.each(function(){
+$dropInputs.each(function () {
   switch (this.value) {
     case this.min:
-      this.previousElementSibling.classList.add('nice-number__button--disabed')
+      this.previousElementSibling.classList.add("nice-number__button_disabed");
       break;
     case this.max:
-      this.nextElementSibling.classList.add('nice-number__button--disabed')
+      this.nextElementSibling.classList.add("nice-number__button_disabed");
       break;
   }
-})
+});
