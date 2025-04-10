431d09794003ac1ea1651bcb5fa1eccc895f83cf
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-10 07:47:28 +0300

    fix: #1 bem naming in the like-button component

--- src/partials/components/like-button/like-button.js
+++ src/partials/components/like-button/like-button.js
@@ -1,8 +1,10 @@
-const $likeBtns = $('.js-like-button___btn');
+const $likeBtns = $(".js-like-button__btn");
 
-function handlelikeBtnsClick(e){
-  e.currentTarget.classList.toggle('like-button___btn--like');
-  e.currentTarget.classList.contains('like-button___btn--like') ? this.textContent++ : this.textContent--
+function handlelikeBtnsClick(e) {
+  e.currentTarget.classList.toggle("like-button__btn_like");
+  e.currentTarget.classList.contains("like-button__btn_like")
+    ? this.textContent++
+    : this.textContent--;
 }
 
-$likeBtns.on('click.likeBtn', handlelikeBtnsClick)
+$likeBtns.on("click.likeBtn", handlelikeBtnsClick);
