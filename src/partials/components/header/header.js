92aee0c7f182ea628870c4dc2988a657c755104b
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-10 11:33:28 +0300

    fix: #1 bem naming in the header component

--- src/partials/components/header/header.js
+++ src/partials/components/header/header.js
@@ -2,9 +2,9 @@ const $burgerBtn = $(".js-header__burger");
 const $headerMenu = $(".js-header__menu");
 
 function handleBurgerClick() {
-  $burgerBtn.toggleClass("header__burger--open");
-  $burgerBtn.children().toggleClass("header__burger-line--open");
-  $headerMenu.toggleClass("header__menu--open");
+  $burgerBtn.toggleClass("header__burger_open");
+  $burgerBtn.children().toggleClass("header__burger-line_open");
+  $headerMenu.toggleClass("header__menu_open");
   $("html").toggleClass("page_menu_open");
 }
 
