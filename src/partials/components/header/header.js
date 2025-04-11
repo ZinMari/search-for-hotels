28b21fc85a2e488f76b9a8dcf7209999ca0488d3
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-11 10:41:31 +0300

    fix: header buttons click

--- src/partials/components/header/header.js
+++ src/partials/components/header/header.js
@@ -1,5 +1,7 @@
 const $burgerBtn = $(".js-header__burger");
 const $headerMenu = $(".js-header__menu");
+const $btnLogin = $(".js-header__login button");
+const $btnRegistration = $(".js-header__registration button");
 
 function handleBurgerClick() {
   $burgerBtn.toggleClass("header__burger_open");
@@ -8,4 +10,14 @@ function handleBurgerClick() {
   $("html").toggleClass("page_menu_open");
 }
 
+function handleBtnLoginClick() {
+  document.location = "./user-login.html";
+}
+
+function handleBtnRegistrationClick() {
+  document.location = "./user-registration.html";
+}
+
 $burgerBtn.on("click.header", handleBurgerClick);
+$btnLogin.on("click.header", handleBtnLoginClick);
+$btnRegistration.on("click.header", handleBtnRegistrationClick);
