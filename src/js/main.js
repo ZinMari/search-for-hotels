0590b191a76842ad12e4e35da42401093b6fb631
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-11 08:54:13 +0300

    feat: #3 remove main bg-slider

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,3 @@ import "./_jquery-libraries.js";
 import "./_components.js";
 
 import "../scss/main.scss";
-
-(function backgroundSlider() {
-  const indexPageSection = document.querySelector(".start");
-  const registrationPageSection = document.querySelector(".user-registration");
-  const signInPageSection = document.querySelector(".user-login");
-
-  const sliderIndex = [
-    "./assets/main-slider-1.jpg",
-    "./assets/main-slider-2.jpg",
-    "./assets/main-slider-3.jpg",
-  ];
-  const sliderRegistrationSignIn = [
-    "./assets/slide-1.jpg",
-    "./assets/slide-2.jpg",
-  ];
-
-  function slider(elem, imagesArr) {
-    let index = 0;
-
-    setInterval(() => {
-      elem.style.backgroundImage = `url('${imagesArr[index]}')`;
-      index++;
-      if (index === imagesArr.length) index = 0;
-    }, 5000);
-  }
-
-  if (indexPageSection) slider(indexPageSection, sliderIndex);
-  if (registrationPageSection)
-    slider(registrationPageSection, sliderRegistrationSignIn);
-  if (signInPageSection) slider(signInPageSection, sliderIndex);
-})();
