7110db4b1de97c8f49ea34ad5d4ad3db52576761
Mari Zin <zin.marie.v@gmail.com>
2025-04-17 12:57:58 +0300

    refactor: #3 update the use of the background-slider component on the user-registration page

--- src/partials/pages/user-registration/user-registration.js
+++
@@ -1,6 +0,0 @@
-import BackgroundSlider from "../../components/background-slider/background-slider";
-
-const ImagesArr = ["./assets/slide-1.jpg", "./assets/slide-2.jpg"];
-const targetElement = document.querySelector(".user-registration");
-
-targetElement && new BackgroundSlider(targetElement, ImagesArr).start();
