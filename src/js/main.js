6c663233bb17490bd3f4a2d3e6d78b676d8a580e
Mari Zin <mari.v.zinkevich@gmail.com>
2025-04-26 10:47:41 +0300

    refactor: #4 imports

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,4 @@ function requireAll(context) {
   return context.keys().map(context);
 }
 
-requireAll(require.context("../partials/", true, /\.scss/));
-requireAll(require.context("../partials/", true, /\.js/));
+requireAll(require.context("../partials/", true, /\.(js|scss)$/));
